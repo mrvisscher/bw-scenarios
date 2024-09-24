46d1b6ae66054bbdb75c63c2bf6748c3fa09b92b
Marin Visscher <m.r.visscher@cml.leidenuniv.nl>
2024-09-24 15:11:19 +0200

    Update `__init__`

--- bw_scenarios/__init__.py
+++ bw_scenarios/__init__.py
@@ -1,10 +1,14 @@
 """bw_scenarios."""
 from bw_scenarios.importer import SDFImporter
+from bw_scenarios.scenario import Scenario
+from bw_scenarios.meta import scenarios
 
 __all__ = (
     "__version__",
     # Add functions and variables you want exposed in `bw_scenarios.` namespace here
-    SDFImporter
+    SDFImporter,
+    Scenario,
+    scenarios,
 )
 
 
