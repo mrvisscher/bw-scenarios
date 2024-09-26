947fb4206bec8b1a4e89c1507394aab7f17da915
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2024-09-26 17:27:33 +0200

    lint importer for tests

--- bw_scenarios/importer.py
+++ bw_scenarios/importer.py
@@ -119,9 +119,7 @@ class SDFImporter:
         if not all([col in df.columns for col in expected_columns]):
             raise ValueError(
                 "Warning: the dataframe does not contain "
-                "the expected columns: {}".format(
-                    expected_columns
-                )
+                "the expected columns: {}".format(expected_columns)
             )
 
         df["from id"] = None
